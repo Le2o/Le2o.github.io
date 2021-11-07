c64072f4a48020d142946f76e1f417165d715f95
Le2o <fai211@yandex.ru>
2021-11-07 21:52:21 +0700

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-toogleLang: function () {
-      this.lang = this.lang === 'en' ? 'ru' : 'en';
-      var queryParams = new URLSearchParams(window.location.search);
-      queryParams.set("lang", this.lang);
-      window.history.replaceState(null, null, "?" + queryParams.toString());
-      this.init();
-    }
